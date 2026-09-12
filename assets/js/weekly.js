96c88641937e1610509bb7d44df5c3be679d3885
GRCorange <2865709743@qq.com>
2026-09-12 16:29:02 +0800

    Fix typo in empty report message

--- assets/js/weekly.js
+++ assets/js/weekly.js
@@ -40,7 +40,7 @@
         '<strong>还没有周报。</strong>' +
         '按上面的说明，在 <code>weekly/posts/</code> 里一个 <code>.md</code> 文件，' +
         '运行 <code>npm run weekly</code>，这里就会出现第一篇。<br>' +
-        '（若你刚 clone 下来，说明生成文件尚未构建。）' +
+        '（若你刚 clone 下来，说明生成文件尚未构建）' +
       '</div>';
     return;
   }
